df7b763039604899245132639ff8b04dceccca9e
LSaldyt <lucassaldyt@gmail.com>
2017-08-21 09:14:18 -0600

    Adds level-diff

--- concept_map.py
+++ concept_map.py
@@ -1,9 +1,10 @@
 import json
 from collections import defaultdict, namedtuple
+from itertools   import zip_longest
 
 from utils import flatten
 
-#from variable_dictionary import VariableDictionary
+from variable_dictionary import VariableDictionary
 
 class ConceptMap(object):
     def __init__(self, filename):
@@ -31,7 +32,7 @@ class ConceptMap(object):
             if isinstance(d, dict):
                 # [[[..]]] potentially infinitely nested lists
                 nested = [recursive_key_values(item) for item in d.values()]
-                return [list(d.items())] + [list(item) for item in zip(*flatten(nested))]
+                return [list(sorted(d.items(), key=lambda t:t[0]))] + [list(item) for item in zip(*flatten(nested))]
             else:
                 return []
         return [[(concept, self.concepts[concept])]] + recursive_key_values(self.concepts[concept])
@@ -42,3 +43,32 @@ class ConceptMap(object):
     def level_values(self, concept):
         return [[v for k, v in kvs] for kvs in self.level_key_values(concept)]
 
+    def difference(self, a, b):
+        kvsA = self.level_key_values(a)
+        kvsB = self.level_key_values(b)
+        variables = VariableDictionary()
+        diff = []
+        for A, B in zip_longest(kvsA, kvsB, fillvalue=None):
+            for (kA, vA) in A:
+                for (kB, vB) in B:
+                    level = []
+                    if isinstance(vA, dict):
+                        vA = None
+                    if isinstance(vB, dict):
+                        vB = None
+                    if kA == kB and vA == vB:
+                        level.append((kA, vA))
+                    elif kA == kB:
+                        variables.add([vA, vB])
+                        level.append((kA, [vA, vB]))
+                    '''
+                    elif vA == vB:
+                        variables.add([kA, kB])
+                        level.append(([kA, kB], vA))
+                    else:
+                        variables.add([kA, kB])
+                        variables.add([vA, vB])
+                        level.append(([kA, kB], [vA, vB]))
+                    '''
+                    diff.append(level)
+        return diff, variables
